fea5eab5cc7fc95305357dc4eb93679d70812e44
sandaluthushan20 <sandaluthushan20@gmail.com>
2024-05-27 09:19:42 +0530

    ransom

---
+++ Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace LAB_07
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.employeeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.databaseDataSet = new LAB_07.DatabaseDataSet();
+            this.employeeTableAdapter = new LAB_07.DatabaseDataSetTableAdapters.EmployeeTableAdapter();
+            this.databaseDataSet1 = new LAB_07.DatabaseDataSet1();
+            this.bookBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.bookTableAdapter = new LAB_07.DatabaseDataSet1TableAdapters.BookTableAdapter();
+            this.databaseDataSet2 = new LAB_07.DatabaseDataSet2();
+            this.bookBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.bookTableAdapter1 = new LAB_07.DatabaseDataSet2TableAdapters.BookTableAdapter();
+            this.databaseDataSet3 = new LAB_07.DatabaseDataSet3();
+            this.employeeBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.employeeTableAdapter1 = new LAB_07.DatabaseDataSet3TableAdapters.EmployeeTableAdapter();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.databaseDataSet4 = new LAB_07.DatabaseDataSet4();
+            this.employeeBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.employeeTableAdapter2 = new LAB_07.DatabaseDataSet4TableAdapters.EmployeeTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.firstNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lastNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // employeeBindingSource
+            // 
+            this.employeeBindingSource.DataMember = "Employee";
+            this.employeeBindingSource.DataSource = this.databaseDataSet;
+            // 
+            // databaseDataSet
+            // 
+            this.databaseDataSet.DataSetName = "DatabaseDataSet";
+            this.databaseDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // employeeTableAdapter
+            // 
+            this.employeeTableAdapter.ClearBeforeFill = true;
+            // 
+            // databaseDataSet1
+            // 
+            this.databaseDataSet1.DataSetName = "DatabaseDataSet1";
+            this.databaseDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // bookBindingSource
+            // 
+            this.bookBindingSource.DataMember = "Book";
+            this.bookBindingSource.DataSource = this.databaseDataSet1;
+            // 
+            // bookTableAdapter
+            // 
+            this.bookTableAdapter.ClearBeforeFill = true;
+            // 
+            // databaseDataSet2
+            // 
+            this.databaseDataSet2.DataSetName = "DatabaseDataSet2";
+            this.databaseDataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // bookBindingSource1
+            // 
+            this.bookBindingSource1.DataMember = "Book";
+            this.bookBindingSource1.DataSource = this.databaseDataSet2;
+            // 
+            // bookTableAdapter1
+            // 
+            this.bookTableAdapter1.ClearBeforeFill = true;
+            // 
+            // databaseDataSet3
+            // 
+            this.databaseDataSet3.DataSetName = "DatabaseDataSet3";
+            this.databaseDataSet3.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // employeeBindingSource1
+            // 
+            this.employeeBindingSource1.DataMember = "Employee";
+            this.employeeBindingSource1.DataSource = this.databaseDataSet3;
+            // 
+            // employeeTableAdapter1
+            // 
+            this.employeeTableAdapter1.ClearBeforeFill = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.firstNameDataGridViewTextBoxColumn,
+            this.lastNameDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.employeeBindingSource2;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 426);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // databaseDataSet4
+            // 
+            this.databaseDataSet4.DataSetName = "DatabaseDataSet4";
+            this.databaseDataSet4.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // employeeBindingSource2
+            // 
+            this.employeeBindingSource2.DataMember = "Employee";
+            this.employeeBindingSource2.DataSource = this.databaseDataSet4;
+            // 
+            // employeeTableAdapter2
+            // 
+            this.employeeTableAdapter2.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // firstNameDataGridViewTextBoxColumn
+            // 
+            this.firstNameDataGridViewTextBoxColumn.DataPropertyName = "FirstName";
+            this.firstNameDataGridViewTextBoxColumn.HeaderText = "FirstName";
+            this.firstNameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.firstNameDataGridViewTextBoxColumn.Name = "firstNameDataGridViewTextBoxColumn";
+            this.firstNameDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // lastNameDataGridViewTextBoxColumn
+            // 
+            this.lastNameDataGridViewTextBoxColumn.DataPropertyName = "LastName";
+            this.lastNameDataGridViewTextBoxColumn.HeaderText = "LastName";
+            this.lastNameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.lastNameDataGridViewTextBoxColumn.Name = "lastNameDataGridViewTextBoxColumn";
+            this.lastNameDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private DatabaseDataSet databaseDataSet;
+        private System.Windows.Forms.BindingSource employeeBindingSource;
+        private DatabaseDataSetTableAdapters.EmployeeTableAdapter employeeTableAdapter;
+        private DatabaseDataSet1 databaseDataSet1;
+        private System.Windows.Forms.BindingSource bookBindingSource;
+        private DatabaseDataSet1TableAdapters.BookTableAdapter bookTableAdapter;
+        private DatabaseDataSet2 databaseDataSet2;
+        private System.Windows.Forms.BindingSource bookBindingSource1;
+        private DatabaseDataSet2TableAdapters.BookTableAdapter bookTableAdapter1;
+        private DatabaseDataSet3 databaseDataSet3;
+        private System.Windows.Forms.BindingSource employeeBindingSource1;
+        private DatabaseDataSet3TableAdapters.EmployeeTableAdapter employeeTableAdapter1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private DatabaseDataSet4 databaseDataSet4;
+        private System.Windows.Forms.BindingSource employeeBindingSource2;
+        private DatabaseDataSet4TableAdapters.EmployeeTableAdapter employeeTableAdapter2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn firstNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lastNameDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB_07
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'databaseDataSet4.Employee' table. You can move, or remove it, as needed.
+            this.employeeTableAdapter2.Fill(this.databaseDataSet4.Employee);
+            // TODO: This line of code loads data into the 'databaseDataSet3.Employee' table. You can move, or remove it, as needed.
+            this.employeeTableAdapter1.Fill(this.databaseDataSet3.Employee);
+            // TODO: This line of code loads data into the 'databaseDataSet2.Book' table. You can move, or remove it, as needed.
+            this.bookTableAdapter1.Fill(this.databaseDataSet2.Book);
+            // TODO: This line of code loads data into the 'databaseDataSet1.Book' table. You can move, or remove it, as needed.
+            this.bookTableAdapter.Fill(this.databaseDataSet1.Book);
+            // TODO: This line of code loads data into the 'databaseDataSet.Employee' table. You can move, or remove it, as needed.
+            this.employeeTableAdapter.Fill(this.databaseDataSet.Employee);
+
+        }
+    }
+}
